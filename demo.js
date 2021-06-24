50577e1de2b23b7f6e9ce4756701df25781be27c
lagelisu <wwdddwwok@126.com>
2021-06-24 11:38:24 +0800

    第一次上传

---
+++ demo.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-24 11:33:02
+ * @LastEditTime: 2021-06-24 11:35:19
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \test-demo\demo.js
+ */
+window.onload = function(){
+    document.getElementById("box").onclick = function(){
+        alert('测试代码')
+    }
+}
